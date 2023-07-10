458b87e490bfbb9db64a0c7ae3655b2a301cdd80
Alexander Hoppe <63847434+LoneWalkerWolf@users.noreply.github.com>
2023-07-10 08:25:01 +0100

    Update ethEspDMX_RDM.h

--- libraries/ethEspDMX_RDM/ethEspDMX_RDM.h
+++ libraries/ethEspDMX_RDM/ethEspDMX_RDM.h
@@ -1,7 +1,10 @@
 /*
-espDMX v2 library
-Copyright (c) 2016, Matthew Tong
-https://github.com/mtongnz/espDMX
+dualETH Ethernet ArtNet Node 
+
+Base Code Copyright (c) 2016, Matthew Tong
+https://github.com/mtongnz/
+Ethernet Implementation Copyright (c) 2023, expanseElectronics Ltd
+https://github.com/expanseElectronics/
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
@@ -9,7 +12,6 @@ later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License along with this program.
 If not, see http://www.gnu.org/licenses/
 */
